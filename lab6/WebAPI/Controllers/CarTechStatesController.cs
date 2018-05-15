0ec4a880605f4d290633484a8921e7765d86921b
Ihar Kamarets <igor.komarets@gmail.com>
2018-05-15 09:59:40 +0300

    Created WebAPI project.

---
+++ lab6/WebAPI/Controllers/CarTechStatesController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/CarTechStates")]
+    public class CarTechStatesController : Controller
+    {
+        readonly CarServiceContext _context;
+
+        public CarTechStatesController(CarServiceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/CarTechStates
+        [HttpGet]
+        public IEnumerable<CarTechState> Get()
+        {
+            return _context
+                .CarTechStates
+                .Include("Car")
+                .Include("Inspector");
+        }
+
+        // GET: api/CarTechStates/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            var state = _context.CarTechStates.FirstOrDefault(s => s.CarTechStateId == id);
+
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+            return new ObjectResult(state);
+        }
+        
+        // POST: api/CarTechStates
+        [HttpPost]
+        public IActionResult Post([FromBody]CarTechState state)
+        {
+            if (state == null)
+            {
+                return BadRequest();
+            }
+
+            _context.CarTechStates.Add(state);
+            _context.SaveChanges();
+
+            return Ok(state);
+        }
+        
+        // PUT: api/CarTechStates/5
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody]CarTechState state)
+        {
+            if (state == null)
+            {
+                return BadRequest();
+            }
+
+            if (!_context.CarTechStates.Any(s => s.CarTechStateId == state.CarTechStateId))
+            {
+                return NotFound();
+            }
+
+            _context.Update(state);
+            _context.SaveChanges();
+
+            return Ok(state);
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var state = _context.CarTechStates.FirstOrDefault(s => s.CarTechStateId == id);
+
+            if (state == null)
+            {
+                return NotFound();
+            }
+
+            _context.CarTechStates.Remove(state);
+            _context.SaveChanges();
+
+            return Ok(state);
+        }
+    }
+}
---
+++ lab6/WebAPI/Models/Inspector.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class Inspector
+    {
+        public int InspectorId { get; set; }
+        [DisplayName("ФИО инспектора")]
+        [Required]
+        public string FullName { get; set; }
+        [DisplayName("Подразделение")]
+        [Required]
+        public string Subdivision { get; set; }
+
+        [Newtonsoft.Json.JsonIgnore]
+        public virtual List<CarTechState> CarTechStates { get; set; }
+
+        public override string ToString()
+        {
+            return $"( ИД = {InspectorId}, Имя = {FullName}, Подразделение = {Subdivision} )";
+        }
+    }
+}
